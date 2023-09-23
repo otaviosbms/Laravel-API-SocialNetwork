e76920f2967aa8c2b26b75e506065f8dac2f3847
Otavio sbms <otaviosbms@hotmail.com>
2023-09-23 02:16:30 -0300

    new project

---
+++ app/Models/Marker.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Marker extends Model
+{
+    use HasFactory;
+
+    public function user()
+{
+    return $this->belongsTo(User::class);
+}
+
+public function marker()
+{
+    return $this->belongsTo(Marker::class);
+}
+
+}
